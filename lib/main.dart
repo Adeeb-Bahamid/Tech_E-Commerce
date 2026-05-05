fa25013fad7c562582a76668a17eda68a35e07a1
Adeeb-Bahamid <adeebbahamid@gmail.com>
2026-05-05 15:00:41 +0300

    after add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+
+
+
+
+
+
+
+
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tech_e_commerce/main_code/shared/services/firebase_helper.dart';
+import 'main_code/admin/home_screen_admin.dart';
+import 'main_code/admin/products.dart';
 import 'main_code/shared/theme/app_theme.dart';
 import 'firebase_options.dart';
-import 'main_code/admin/dashboard.dart';
+// import 'main_code/admin/dashboard.dart';
 import 'main_code/customer/home_screen_customer.dart';
 import 'main_code/screens/login.dart';
 
@@ -45,45 +57,46 @@ class _AuthGetState extends State<AuthGet> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, authSnapshot) {
-            if (authSnapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
+        body: HomeScreenAdmin(),
+        // body: StreamBuilder<User?>(
+        //   stream: FirebaseAuth.instance.authStateChanges(),
+        //   builder: (context, authSnapshot) {
+        //     if (authSnapshot.connectionState == ConnectionState.waiting) {
+        //       return const Center(child: CircularProgressIndicator());
+        //     }
 
-            if (authSnapshot.hasError) {
-              return const Center(
-                  child: Text('An error occurred, please try again.'));
-            }
+        //     if (authSnapshot.hasError) {
+        //       return const Center(
+        //           child: Text('An error occurred, please try again.'));
+        //     }
 
-            if (authSnapshot.hasData && authSnapshot.data != null) {
-              final uid = authSnapshot.data!.uid;
-              return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                future: _firebaseHelper.getCollectionUser(
-                    collection: 'Users', uid: uid),
-                builder: (context, userSnapshot) {
-                  if (userSnapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
+        //     if (authSnapshot.hasData && authSnapshot.data != null) {
+        //       final uid = authSnapshot.data!.uid;
+        //       return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        //         future: _firebaseHelper.getCollectionUser(
+        //             collection: 'Users', uid: uid),
+        //         builder: (context, userSnapshot) {
+        //           if (userSnapshot.connectionState == ConnectionState.waiting) {
+        //             return const Center(child: CircularProgressIndicator());
+        //           }
 
-                  if (userSnapshot.hasData && userSnapshot.data!.exists) {
-                    final data = userSnapshot.data!.data();
-                    final role = data?['role'];
-                    if (role == 'admin') {
-                      return const Dashboard();
-                    } else {
-                      return const HomeScreenCustomer();
-                    }
-                  }
-                  return const LoginPage();
-                },
-              );
-            }
+        //           if (userSnapshot.hasData && userSnapshot.data!.exists) {
+        //             final data = userSnapshot.data!.data();
+        //             final role = data?['role'];
+        //             if (role == 'admin') {
+        //               return const Products();
+        //             } else {
+        //               return const HomeScreenCustomer();
+        //             }
+        //           }
+        // return const LoginPage();
+        //         },
+        //       );
+        //     }
 
-            return const LoginPage();
-          },
-        ),
+        //     return const LoginPage();
+        //   },
+        // ),
       ),
     );
   }
